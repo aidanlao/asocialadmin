cc05392fac86461b3514c9b4051ab22d66b2ea19
Aidan Lao <laoaidan@gmail.com>
2024-04-02 20:20:40 -0300

    Added functionality for adding businessses

--- src/components/Businesses/addBusiness.js
+++ src/components/Businesses/addBusiness.js
@@ -1,7 +1,9 @@
+import { useState } from "react";
+import { useAddBusiness } from "../../hooks/business";
 export default function AddBusiness() {
 
     const [inputs, setInputs] = useState({});
-
+    const { addBusiness, isAdding } = useAddBusiness();
     function handleChange(e) {
         const name = e.target.name;
         const value = e.target.value;
@@ -9,21 +11,21 @@ export default function AddBusiness() {
         
     }
     function onCompletion(message) {
-        console.log("complete " + message);
-        var p = document.createElement("p");
-        p.className = "dissapear";
-        document.getElementById("message")?.remove();
-        p.id = "message";
-        p.innerHTML = message;
-        document.getElementById("addRewardForm").appendChild(p);
-        triggerUpdate({});
+        // console.log("complete " + message);
+        // var p = document.createElement("p");
+        // p.className = "dissapear";
+        // document.getElementById("message")?.remove();
+        // p.id = "message";
+        // p.innerHTML = message;
+        // document.getElementById("addRewardForm").appendChild(p);
+        // triggerUpdate({});
     }
     const handleSubmit = (event) => {
         event.preventDefault();
         const data = new FormData(event.target);
         let formObject = Object.fromEntries(data.entries());
         // change this to add business
-        addReward({ ...formObject}, onCompletion);
+        addBusiness({ ...formObject}, onCompletion);
     }
     return (
 <>
@@ -38,7 +40,15 @@ export default function AddBusiness() {
                     value={inputs.businessName} 
                     onChange={handleChange} />
                 </div>
-
+                <div>
+                    <label>Business ID</label>
+                    <input 
+                    type="text"
+                    name="businessID"
+                    value={inputs.businessID} 
+                    onChange={handleChange} />
+                </div>
+                <input value="Add Business" type="submit" class="submitButton" />
             </form>
         </div>
 </>
--- src/components/Businesses/businesses.js
+++ src/components/Businesses/businesses.js
@@ -1,19 +1,21 @@
 import { useState } from "react";
 import BusinessList from "../businesses/businesslist";
+import AddBusiness from "./addBusiness";
 
 export default function BusinessManager() {
     const [currentBusiness, setCurrentBusiness] = useState({});
     return (
         <>
-            <div className="businessWrapper">
+            <div className="cardWrapper">
+                
+            <h1>Business Manager </h1>
                 <div className="businessCard dynamicmd card flex">
-                    <div className="businessSection w30md">
-                        <h1>Businesses</h1>
+                    <div className="businessSection w30md ">
                         <BusinessList currentBusiness={currentBusiness} setCurrentBusiness={setCurrentBusiness} />
 
                     </div>
-                    <div className="editSection w70md">
-
+                    <div className="editSection w70md ">
+                        <AddBusiness />
                     </div>
                 </div>
             </div>
